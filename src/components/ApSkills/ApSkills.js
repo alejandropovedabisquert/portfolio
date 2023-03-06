4b726f219cef5a23466fc9f13a98a9ee63bf1e7c
alex2001bis <alex2001bis@gmail.com>
2023-03-06 16:09:16 +0100

    diversos cambios

--- src/components/ApSkills/ApSkills.js
+++ src/components/ApSkills/ApSkills.js
@@ -7,7 +7,7 @@ const ApSkills = (passData) =>{
 
     const listSkills = skills && skills.map((skill) =>
         <ApCard 
-            classImage="w-20 mt-1 ml-1 sm:w-24 lg:w-16"
+            classImage="mt-1 ml-1 w-16"
             classCard="skills flex items-center border-b-2 border-b-secondary-color shadow-lg"
             classContent="relative text-xl w-full"
             key={skill.id} 
@@ -20,7 +20,7 @@ const ApSkills = (passData) =>{
         <div id="skills" className="transition-all container mx-auto text-center">
             <div className="p-8">
                 <h1 className={titleClassStyle}>Mis conocimientos</h1>
-                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+                <div className="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
                     {listSkills}
                 </div> 
             </div>
