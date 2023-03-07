d20bb28bee5de966730545c8509ab3ba9fb0c7f7
alex2001bis <alex2001bis@gmail.com>
2023-03-07 12:29:28 +0100

    Diversos cambios e implementacion emailjs

--- src/components/ApAbout/ApAbout.js
+++ src/components/ApAbout/ApAbout.js
@@ -2,7 +2,7 @@ import "./ApAbout.css"
 const ApAbout = (passData) =>{
     const about = passData.data.about
     const titleClassStyle = passData.titleClassStyle
-    const buttonClassStyle = "border-2 border-secondary-color p-2 px-4 mt-4 inline-block rounded shadow-md transition-all font-bold duration-300 relative hover:text-white before:content-[''] before:absolute before:top-0 before:left-0 before:h-full before:content-[''] before:absolute before:top-0 before:left-0 before:w-0 before:-z-10 before:bg-primary-color before:transition-all before:duration-300 before:hover:w-full"
+    const buttonClassStyle = "border-2 border-secondary-color p-2 px-4 mt-4 inline-block rounded-md shadow-md transition-all font-bold duration-300 relative hover:text-white before:content-[''] before:absolute before:top-0 before:left-0 before:h-full before:content-[''] before:absolute before:top-0 before:left-0 before:w-0 before:-z-10 before:bg-primary-color before:transition-all before:duration-300 before:hover:w-full"
     const profile = passData.data.logos && <img className="rounded-md" src={passData.data.logos && passData.data.logos.perfil} alt="Perfil" width="300px"/>
     return(
         <div id="about" className="container transition-all mx-auto text-center grid grid-cols-1 md:grid-cols-3">
--- src/components/ApContact.js
+++ src/components/ApContact.js
@@ -1,10 +1,26 @@
+import { useRef } from "react"
+import emailjs from '@emailjs/browser'
+
 
 const ApContact = (passData) =>{
     const titleClassStyle = passData.titleClassStyle
+
+    const form = useRef()
+    const sendEmail = (e) =>{
+        e.preventDefault()
+        emailjs.sendForm('service_ybqrbf6', 'template_uf8oruc', form.current, 'RzrOefDvuoF6uMf1O')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            })
+        e.target.reset()
+    }
+
     return(
-        <div id="contact" className="text-center">
+        <div id="contact" className="text-center container mx-auto p-8">
             <h1 className={titleClassStyle}>Contacto</h1>
-            <form>
+            <form className="grid p-8 w-9/12 h-96 box-content shadow-md rounded-md mx-auto bg-secondary-color transition-all lg:w-[50em]" ref={form} onSubmit={sendEmail}>
                 <label>Name</label>
                 <input type="text" name="user_name" />
                 <label>Email</label>
