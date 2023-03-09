d7194fd81089a2b14c2011e4b3730541671e6802
alex2001bis <alex2001bis@gmail.com>
2023-03-09 12:13:12 +0100

    update contacto

--- src/components/ApContact.js
+++ src/components/ApContact.js
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react"
 import emailjs from '@emailjs/browser'
-import { FaRegCheckCircle } from "react-icons/fa";
+import { FaRegCheckCircle, FaSpinner } from "react-icons/fa";
 
 
 
@@ -11,24 +11,21 @@ const ApContact = (passData) =>{
     const textAreaClass = "block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 focus:outline-none focus:ring-2 focus:ring-secondary-color"
     const form = useRef() 
     const [statusMessage, setStatusMessage] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-    const prueba = ()=>{
-        setStatusMessage({type: "success" , message: "El email se envió correctamente", opacity: true});
-        setTimeout(() => setStatusMessage({type: "success" , message: "El email se envió correctamente", opacity: false}), 5000)
-        setTimeout(() => setStatusMessage([]), 6000)
-    }
   
     const sendEmail = (e) =>{
+        setIsLoading(true)
         e.preventDefault()
         emailjs.sendForm('service_ybqrbf6', 'template_uf8oruc', form.current, 'RzrOefDvuoF6uMf1O')
             .then((result) => {
-                console.log(result.text);
+                setIsLoading(false)
                 e.target.reset()
                 setStatusMessage({type: "success" , message: "El email se envió correctamente", opacity: true});
-                setTimeout(() => setStatusMessage({type: "success" , message: "El email se envió correctamente", opacity: false}), 2000)
-                setTimeout(() => setStatusMessage([]), 3000)
+                setTimeout(() => setStatusMessage({type: "success" , message: "El email se envió correctamente", opacity: false}), 5000)
+                setTimeout(() => setStatusMessage([]), 6000)
             }, (error) => {
-                console.log(error.text);
+                setIsLoading(false)
                 setStatusMessage({type: "error" , message: `Hubo un error en el envio (${error.text})`});
             })
     }
@@ -36,6 +33,9 @@ const ApContact = (passData) =>{
     return(
         <div id="contact" className="text-left container relative mx-auto p-8 md:px-0">
             <div className="py-8 lg:pb-8 px-4 md:px-8 mx-auto max-w-screen-md bg-white rounded-md drop-shadow-lg">
+                {isLoading 
+                    ? <div className="absolute flex bg-gray-300 opacity-40 top-0 left-0 rounded-md z-10 bg- w-full h-full"><FaSpinner className="animate-spin h-16 w-16 mx-auto self-center"/></div>
+                    :null}
              
                 <h1 className={`${titleClassStyle} pb-4 md:pb-8`}>Contáctame</h1>
                 <form ref={form} onSubmit={sendEmail} className="space-y-8">
@@ -50,11 +50,9 @@ const ApContact = (passData) =>{
                     </div>
                     <button type="submit" className={buttonClassStyle}>Enviar</button>
                 </form>
-                <button onClick={prueba}>Pureba</button>
                 <div className={`${statusMessage.opacity === true ? 'opacity-100': 'opacity-0'} duration-500 transition-all p-4 bg-green-500 absolute -bottom-7 mx-auto left-0 right-0 drop-shadow-lg rounded-md w-auto md:w-80`}>
                     {statusMessage.type === 'success' ? <div className="flex items-center"><FaRegCheckCircle className="mr-2"/>{statusMessage.message}</div>:null}
                 </div>
-                {console.log(statusMessage)}
             </div>
         </div>
     )
