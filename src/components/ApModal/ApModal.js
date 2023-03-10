cc24c921493a777be67d3a2e5d736fe8d8bbea8f
alex2001bis <alejandro2001bis@gmail.com>
2023-03-10 17:53:15 +0100

    arreglos modal

--- src/components/ApModal/ApModal.js
+++ src/components/ApModal/ApModal.js
@@ -6,7 +6,7 @@ const ApModal = (props) => {
 
     return(
         <div className="modal fixed top-0 left-0 right-0 flex items-center justify-center bottom-0 bg-gray-400 z-50">
-            <div className="modal-content bg-white grid grid-cols-1 md:grid-cols-2 m-10 md:m-20 overflow-auto h-80vh rounded-md">
+            <div className="modal-content bg-white grid grid-cols-1 md:grid-cols-2 m-10 md:m-20 overflow-auto h-128 rounded-md">
                 <div className="modal-content-left p-8">
                     <div className="modal-image">
                         <img src={props.image} alt={props.title} className="rounded-md w-128"/>
