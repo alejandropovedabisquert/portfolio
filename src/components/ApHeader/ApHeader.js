102f3c94790e133a35719cde4354a4f4da65158e
alex2001bis <alex2001bis@gmail.com>
2023-03-13 15:46:34 +0100

    boldear texto titulo

--- src/components/ApHeader/ApHeader.js
+++ src/components/ApHeader/ApHeader.js
@@ -6,7 +6,7 @@ const ApHeader = (passData)=>{
     return(
         <div id="header">
             <div style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.50), rgba(0, 0, 0, 0.50)),url(${logos && logos.fondoTeclado})`}} className="bg-cover bg-fixed bg-center flex flex-col h-screen text-center items-center justify-center">
-                <h1 className="bg-gradient-to-r from-primary-color via-secondary-color to-primary-color text-transparent bg-clip-text animate-gradient-animation font-semibold sm:tracking-widest text-6xl md:text-8xl">
+                <h1 className="bg-gradient-to-r from-primary-color via-secondary-color to-primary-color text-transparent bg-clip-text animate-gradient-animation font-bold md:font-semibold sm:tracking-widest text-6xl md:text-8xl">
                     {header && header.name}
                     <span> {header && header.surname}</span>
                 </h1>
