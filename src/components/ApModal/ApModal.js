60b8d742119198ef7e62776f8c15123eaf042f2c
alex2001bis <alex2001bis@gmail.com>
2023-04-05 15:27:18 +0200

    arreglos modal

--- src/components/ApModal/ApModal.js
+++ src/components/ApModal/ApModal.js
@@ -7,6 +7,7 @@ const ApModal = (props) => {
     const open = props.open
     const setOpen = props.setOpen
     const buttonClassStyle= props.buttonClassStyle
+    const closeButton = "border-2 cursor-pointer absolute rounded-bl-md rounded-tr-md sm:rounded-tl-md right-0 top-0 sm:bottom-0 px-2 border-secondary-color shadow-md transition-all font-bold duration-300 hover:text-white before:content-[''] before:absolute before:top-0 before:left-0 before:h-full before:content-[''] before:absolute before:top-0 before:left-0 before:w-0 before:-z-10 before:bg-primary-color before:transition-all before:duration-300 before:hover:w-full"
     useEffect(()=>{
         const detectKeyDown= (e) =>{
             if (e.key === "Escape") {
@@ -20,7 +21,7 @@ const ApModal = (props) => {
         <div className="fixed top-0 left-0 right-0 bottom-0 z-50">
             <div className="fixed top-0 left-0 right-0 flex items-center justify-center bottom-0 bg-light-gray" onClick={() => setOpen(!open)}></div>
             <div className="modal flex items-center justify-center h-full">
-                <div className="modal-content flex flex-wrap basis-5/6 md:basis-3/4 xl:basis-320 bg-white overflow-auto h-96 sm:h-160 md:h-auto rounded-md z-50 ultrawide:w-464">
+                <div className="modal-content relative flex flex-wrap basis-5/6 md:basis-3/4 xl:basis-320 bg-white overflow-auto h-96 sm:h-160 md:h-auto rounded-md z-50 ultrawide:w-464">
                     <div className="modal-content-left xl:basis-4/6 p-8">
                         <div className="modal-image">
                             <a href={props.image} target="_blank" rel="noreferrer"><img src={props.image} alt={props.title} className="rounded-md"/></a>
@@ -50,6 +51,7 @@ const ApModal = (props) => {
                             }
                         </div>
                     </div>
+                    <div className={closeButton} onClick={() => setOpen(!open)} >x</div>
                 </div>
             </div>
         </div>
